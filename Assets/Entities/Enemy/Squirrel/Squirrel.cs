6c7b98179d0c9d88f8b21f8a543199643e68ffbb
Brian <yebri@umichedu>
2016-10-15 19:33:39 -0400

    Finished level 9 and established wolves

---
+++ Assets/Entities/Enemy/Squirrel/Squirrel.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Squirrel : Enemy {
+
+    public Direction squirrelDir;
+	int moveDistance;
+
+	// Use this for initialization
+	protected override void Start () {
+		moveDistance = 3;
+        base.Start();
+		if (squirrelDir == Direction.NORTH)
+            SetRotation(0);
+		else if (squirrelDir == Direction.EAST)
+            SetRotation(90);
+		else if (squirrelDir == Direction.SOUTH)
+            SetRotation(180);
+        else SetRotation(270);
+    }
+
+    public override void Move()
+    {
+        IntVector2 movePos = pos;
+        IntVector2 initialPos = pos;
+        if (!alarmRaised)
+        {           
+            if (GetDirectionFacing() == Direction.EAST)
+            {
+                movePos.x += 1;
+                MoveIfAble(movePos);
+            }
+            else if (GetDirectionFacing() == Direction.WEST)
+            {
+                movePos.x -= 1;
+                MoveIfAble(movePos);
+            }
+            else if (GetDirectionFacing() == Direction.NORTH)
+            {
+                movePos.y += 1;
+                MoveIfAble(movePos);
+            }
+            else if (GetDirectionFacing() == Direction.SOUTH)
+            {
+                movePos.y -= 1;
+                MoveIfAble(movePos);
+            }
+            //If unable to move, reverse direction
+            if (pos == initialPos)
+            {
+                SetRotation(GetRotation() + 180);
+            }
+            if (CanSeePlayer(GetDirectionFacing()))
+            {
+                RaiseAlarm();
+            }
+        }
+        else
+        {
+            ChasePlayer();
+            //If he only turned, also move
+            if (initialPos == pos) ChasePlayer();
+        }
+    }
+}
