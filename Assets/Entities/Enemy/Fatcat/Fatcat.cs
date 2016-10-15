f610a7098e45895ab41ac46c16ecefa38bcfc046
Daniel Engwis <dangwis@umich.edu>
2016-10-15 12:24:48 -0400

    Directional fatcat prefabs

--- Assets/Entities/Enemy/Fatcat/Fatcat.cs
+++ Assets/Entities/Enemy/Fatcat/Fatcat.cs
@@ -2,13 +2,21 @@
 using System.Collections;
 
 public class Fatcat : Enemy {
-
+    public Direction fatCatDir;
 	// Use this for initialization
-	protected override void Start() { 
+	protected override void Start() {
+        if (fatCatDir == Direction.NORTH)
+            SetRotation(0);
+        else if (fatCatDir == Direction.EAST)
+            SetRotation(90);
+        else if (fatCatDir == Direction.SOUTH)
+            SetRotation(180);
+        else SetRotation(270);
+
 		base.Start();
 	}
 
-	//Sentinel looks around, doesn't move unless he sees you
+	//FatCat looks in one direction, doesn't move unless he sees you
 	public override void Move()
 	{
         if (CanSeePlayer(GetDirectionFacing()))
