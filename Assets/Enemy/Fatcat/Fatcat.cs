172af391dfb2ad0f031e12edb73e730069019336
Brian <yebri@umichedu>
2016-10-15 00:59:51 -0400

    Added some music

---
+++ Assets/Enemy/Fatcat/Fatcat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fatcat : Enemy {
+
+
+	public Fatcat(Direction start_direction) {
+		currentDirection = start_direction;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	//Sentinel looks around, doesn't move unless he sees you
+	public override void Move()
+	{
+		if (alarmRaised)
+		{
+			ChasePlayer();
+		}
+	}
+}
