b79bb36ba433e79f877551c1e14f4e42c2659b90
TBartl <tbartl@umich.edu>
2016-10-15 13:07:42 -0400

    Started working on dialogue, changed chase music

---
+++ Assets/UI/Dialogue/HoverAbovePlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverAbovePlayer : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (PlayerMovement.S)
+		{
+			this.transform.position = PlayerMovement.S.transform.position + Vector3.up * 1.5f;
+		}
+	}
+}
