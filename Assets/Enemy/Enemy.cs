b56306068911ab7711164beda6789a56e52c513c
Daniel Engwis <dangwis@umich.edu>
2016-10-15 00:19:35 -0400

    made chase enemy a part of base class

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour {
     public int sightDistance = 2;
     public IntVector2 pos;
     public static bool _alarmRaised = false;
+    public Direction currentDirection;
 
 	// Use this for initialization
 	void Start () {
@@ -34,6 +35,76 @@ public class Enemy : MonoBehaviour {
             else return false;
         }
         else return false;       
+    }
+
+    public virtual void ChasePlayer()
+    {
+        IntVector2 playerPos = PlayerMovement.Player.position;
+        float xDif, yDif;
+        xDif = Mathf.Abs(playerPos.x - pos.x);
+        yDif = Mathf.Abs(playerPos.y - pos.y);
+        if (xDif >= yDif)
+        {
+            if (playerPos.x < pos.x)
+            {
+                if (currentDirection != Direction.WEST)
+                    currentDirection = Direction.WEST;
+                else
+                {
+                    IntVector2 movePos = pos;
+                    movePos.x -= 1;
+                    if (LevelManager.S.realData[movePos.x, movePos.y].passable)
+                    {
+                        LevelManager.S.MoveSomething(pos, movePos);
+                    }
+                }
+            }
+            else
+            {
+                if (currentDirection != Direction.EAST)
+                    currentDirection = Direction.EAST;
+                else
+                {
+                    IntVector2 movePos = pos;
+                    movePos.x += 1;
+                    if (LevelManager.S.realData[movePos.x, movePos.y].passable)
+                    {
+                        LevelManager.S.MoveSomething(pos, movePos);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (playerPos.y < pos.y)
+            {
+                if (currentDirection != Direction.SOUTH)
+                    currentDirection = Direction.SOUTH;
+                else
+                {
+                    IntVector2 movePos = pos;
+                    movePos.y -= 1;
+                    if (LevelManager.S.realData[movePos.x, movePos.y].passable)
+                    {
+                        LevelManager.S.MoveSomething(pos, movePos);
+                    }
+                }
+            }
+            else
+            {
+                if (currentDirection != Direction.NORTH)
+                    currentDirection = Direction.NORTH;
+                else
+                {
+                    IntVector2 movePos = pos;
+                    movePos.y += 1;
+                    if (LevelManager.S.realData[movePos.x, movePos.y].passable)
+                    {
+                        LevelManager.S.MoveSomething(pos, movePos);
+                    }
+                }
+            }
+        }
     }
 
     public static bool alarmRaised
