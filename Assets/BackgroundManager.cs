0da438d7304e428d651e975fdd41be792aa9ce82
TBartl <tbartl@umich.edu>
2016-10-16 17:15:56 -0400

    Made the background gradient a bit lighter, will go through each scene and update once I pull

---
+++ Assets/BackgroundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+	static bool useAlt = false;
+	public GameObject background1;
+	public GameObject background2;
+
+	void Start()
+	{
+		if (useAlt)
+		{
+			background1.SetActive(false);
+			background2.SetActive(true);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			useAlt = !useAlt;
+			if (useAlt)
+			{
+				background1.SetActive(false);
+				background2.SetActive(true);
+			} else
+			{
+				background1.SetActive(true);
+				background2.SetActive(false);
+			}
+		}
+	}
+}
