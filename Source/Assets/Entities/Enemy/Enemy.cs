8820f305ddfdc0bfb5f191cafb1bebf0cf560115
TBartl <tbartl@umich.edu>
2017-09-21 12:59:48 -0400

    Moved everything into Source folder, deleted 494 content, upgraded to Unity 2017

---
+++ Source/Assets/Entities/Enemy/Enemy.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : BaseMovement {
+
+	public bool turnsFirst = true;
+    public static bool _alarmRaised = false;
+	
+	protected override void Start()
+	{
+		base.Start();
+		alarmRaised = false;
+	}
+
+    public virtual void Move()
+    {
+
+    }
+
+	public bool CanSeePlayerDirectionless()
+	{
+		if (Game_Manager.undetectable)
+			return false;
+
+		return (LevelManager.S.realData[pos.x, pos.y].light == LightAmount.lit &&
+			 LevelManager.S.realData[PlayerMovement.S.pos.x, PlayerMovement.S.pos.y].visionBlock == VisionBlock.open) ;
+	}
+
+	public virtual bool CanSeePlayer(Direction dir)
+    {
+		if (Game_Manager.undetectable)
+			return false;
+
+		if (CanSeePlayerDirectionless())
+        {
+            if (dir == Direction.NORTH && PlayerMovement.S.pos.y > pos.y)
+                return true;
+            else if (dir == Direction.SOUTH && PlayerMovement.S.pos.y < pos.y)
+                return true;
+            else if (dir == Direction.EAST && PlayerMovement.S.pos.x > pos.x)
+                return true;
+            else if (dir == Direction.WEST && PlayerMovement.S.pos.x < pos.x)
+                return true;
+            else return false;
+        }
+        else return false;       
+    }
+
+    public virtual void ChasePlayer()
+    {
+        IntVector2 playerPos = PlayerMovement.S.pos;
+        float xDif, yDif;
+        xDif = Mathf.Abs(playerPos.x - pos.x);
+        yDif = Mathf.Abs(playerPos.y - pos.y);
+        IntVector2 movePos = pos;
+        if (xDif >= yDif)
+        {
+            if (playerPos.x < pos.x)
+            {
+				if (turnsFirst && GetRotation() != 270)
+					SetRotation(270);
+				else
+				{
+					movePos.x -= 1;
+					MoveIfAble(movePos);
+				}
+            }
+            else
+            {
+                if (turnsFirst && GetRotation() != 90)
+					SetRotation(90);
+				else
+                {
+                    movePos.x += 1;
+					MoveIfAble(movePos);
+				}
+            }
+        }
+        else
+        {
+            if (playerPos.y < pos.y)
+            {
+				if (turnsFirst && GetRotation() != 180)
+					SetRotation(180);
+				else
+                {
+                    movePos.y -= 1;
+					MoveIfAble(movePos);
+                }
+            }
+            else
+            {
+				if (turnsFirst && GetRotation() != 0)
+					SetRotation(0);
+				else
+                {
+                    
+                    movePos.y += 1;
+					MoveIfAble(movePos);
+				}
+            }
+        }
+    }
+
+    public static bool alarmRaised
+    {
+        get
+        {
+            return _alarmRaised;
+        }
+        set
+        {
+            _alarmRaised = value;
+        }
+    }
+
+	protected void RaiseAlarm()
+	{
+		alarmRaised = true;
+		AudioManager.S.StartChase();
+		SoundManager.SM.StartWhistle();
+
+		// if in credit level, should not flash red
+		if (AudioManager.S.musiclevel != 3)
+			Redpanel.RP.StartFlash();
+
+		StartCoroutine(Hop());
+
+		foreach (GameObject g in GameObject.FindGameObjectsWithTag("Vision"))
+		{
+			g.SetActive(false);
+		}
+		StartCoroutine(PreserveThisVisionForASecond());
+		
+	}
+
+	IEnumerator Hop()
+	{
+		float length = .3f;
+		for (float f = 0; f < length; f += Time.deltaTime)
+		{
+			float percent = f / length;
+			transform.position = (Vector3)pos + .6f * Vector3.up * Mathf.Sin(percent * Mathf.PI);
+			yield return null;
+		}
+		this.transform.position = (Vector3)pos;
+	}
+
+	IEnumerator PreserveThisVisionForASecond()
+	{
+		GameObject vision = this.transform.Find("Vision").gameObject;
+
+		vision.SetActive(true);
+		for (float f = 0; f < 1f; f+= Time.deltaTime)
+			yield return null;
+
+		vision.SetActive(false);
+	}
+}
---
+++ Source/Assets/Entities/Enemy/Mole/Mole.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mole : Enemy
+{
+
+	GameObject meshGO;
+
+	enum StepInProcess
+	{
+		Arrived,
+		Up
+	}
+
+	public Direction moleDir;
+	public IntVector2[] hillLocations;
+
+	int currentHillIndex;
+	StepInProcess curStep;
+
+	// Use this for initialization
+	protected override void Start()
+	{
+		base.Start();
+		if (moleDir == Direction.NORTH)
+			SetRotation(0);
+		else if (moleDir == Direction.EAST)
+			SetRotation(90);
+		else if (moleDir == Direction.SOUTH)
+			SetRotation(180);
+		else SetRotation(270);
+		if (hillLocations == null)
+		{
+			hillLocations = new IntVector2[1];
+			hillLocations[1] = pos;
+		}
+		currentHillIndex = 0;
+		curStep = StepInProcess.Arrived;
+		meshGO = transform.Find("Mole").gameObject;
+	}
+
+	public override void Move()
+	{
+		if (!alarmRaised)
+		{
+			if (curStep == StepInProcess.Arrived)
+			{
+				//Animate it coming out of hole
+				//------------------------------------
+				curStep = StepInProcess.Up;
+			}
+			else if (curStep == StepInProcess.Up)
+			{
+				//Animate it going into hole
+				//----------------------------------
+
+				IntVector2 initialPos = pos;
+				currentHillIndex++;
+				if (currentHillIndex >= hillLocations.Length)
+				{
+					currentHillIndex = 0;
+				}
+				IntVector2 movePos = hillLocations[currentHillIndex];
+				if (movePos != pos) //there are more than one mole hill
+				{
+					MoveIfAble(movePos);
+					if (pos != initialPos) // you moved, so change to arrived
+					{
+						curStep = StepInProcess.Arrived;
+					}
+				}
+				else // there is only one mole hill
+				{
+					curStep = StepInProcess.Arrived;
+				}
+
+			}
+			if (CanSeePlayer(GetDirectionFacing()))
+			{
+				RaiseAlarm();
+			}
+		}
+	}
+
+	protected override void MoveIfAble(IntVector2 newPos)
+	{
+		if (LevelManager.S.InBounds(newPos) &&
+			LevelManager.S.realData[newPos.x, newPos.y].passable == true &&
+			(LevelManager.S.realData[newPos.x, newPos.y].occupant == null || (LevelManager.S.realData[newPos.x, newPos.y].occupant != null && LevelManager.S.realData[newPos.x, newPos.y].occupant.tag == "Collectable")))
+		{
+			Move(newPos);
+		}
+	}
+
+	protected override void Move(IntVector2 newPos)
+	{
+		LevelManager.S.MoveOccupant(pos, newPos);
+		StartCoroutine(AnimateMole(pos, newPos));
+		pos = newPos;
+	}
+
+	IEnumerator AnimateMole(IntVector2 fromPos, IntVector2 toPos)
+	{
+		this.transform.position = (Vector3)fromPos;
+		float length = smoothMoveTime;
+		for (float f = 0; f < length; f += Time.deltaTime)
+		{
+			meshGO.transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.down * .5f, f / length);
+			yield return null;
+		}
+		this.transform.position = (Vector3)toPos;
+		for (float f = 0; f < length; f += Time.deltaTime)
+		{
+			meshGO.transform.localPosition = Vector3.Lerp(Vector3.down * .5f, Vector3.zero, f / length);
+			yield return null;
+		}
+	}
+}
